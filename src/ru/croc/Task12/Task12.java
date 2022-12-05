38a38467a55b391b88f8643a46450348a8381641
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-05 19:55:49 +0300

    Update Task12.java

--- src/ru/croc/Task12/Task12.java
+++ src/ru/croc/Task12/Task12.java
@@ -5,20 +5,18 @@ import java.util.*;
 public class Task12 {
     public static void main(String[] args) throws Exception {
         Comments c = new Comments();
-        List<String> comments = new ArrayList<>();
-        Set<String> badwords = new HashSet<>();
         Scanner scanner = new Scanner(System.in);
         String fpath = "";
         System.out.print("Введите имя файла с комментариями: ");
         fpath = scanner.nextLine();
-        c.fillComments(comments, fpath);
+        c.fillComments(fpath);
         System.out.print("Введите имя файла с черным списком: ");
         fpath = scanner.nextLine();
-        c.fillVlackList(badwords, fpath);
+        c.fillBlackList(fpath);
         System.out.print("Введите имя файла для записи результатов: ");
         fpath = scanner.nextLine();
-        c.filterComments(comments, badwords);
-        c.writeResults(comments, fpath);
+        c.filter();
+        c.writeResults(fpath);
         scanner.close();
     }
 }
