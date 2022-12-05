f429e170b311074ffc0e02bbbdb41cbfc05dcd7e
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-06 02:03:19 +0300

    Create Driver.java

---
+++ src/ru/croc/Task16/Driver.java
@@ -0,0 +1,46 @@
+package ru.croc.Task16;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Driver {
+    private String name;
+    private double xcord;
+    private double ycord;
+    private Car c;
+    public Driver(String _name, double x, double y, Car _c){
+        this.name = _name;
+        this.xcord = x;
+        this.ycord = y;
+        this.c = _c;
+    }
+    @Override
+    public String toString(){
+        return name + ", [" + xcord + "; " + ycord + "]: " + c;
+    }
+    public List<String> getFacilities(){
+        return c.getFacilities();
+    }
+    public double getXcord(){
+        return xcord;
+    }
+    public double getYcord() {
+        return ycord;
+    }
+    public String getType(){
+        return c.getType();
+    }
+    public int getNum(){
+        return c.getNum();
+    }
+    public static Comparator<Driver> driverComparator = (d1, d2) -> {
+        if (d1.getNum() == d2.getNum()) {
+            return 0;
+        } else if (d1.getNum() > d2.getNum()) {
+            return 1;
+        } else if (d1.getNum() < d2.getNum()) {
+            return -1;
+        }
+        return 0;
+    };
+}
