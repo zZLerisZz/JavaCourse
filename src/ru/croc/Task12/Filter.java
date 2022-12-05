b897ecc4d4b8eb840268c796ee4e631423cb7c19
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-05 19:54:58 +0300

    Create Filter.java

---
+++ src/ru/croc/Task12/Filter.java
@@ -0,0 +1,19 @@
+package ru.croc.Task12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class Filter implements BlackListFilter{
+    @Override
+    public void filterComments(List<String> comments, Set<String> blackList) {
+        List<String> badcomments = new ArrayList<>();
+        for(var item:comments){
+            for(var it:blackList){
+                if(item.contains(it))
+                    badcomments.add(item);
+            }
+        }
+        comments.removeAll(badcomments);
+    }
+}
