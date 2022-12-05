1065e7b25ba27af4397b20db9b5b5938a2f4b0a1
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-05 21:50:39 +0300

    Create Task14.java

---
+++ src/ru/croc/Task14/Task14.java
@@ -0,0 +1,22 @@
+package ru.croc.Task14;
+
+import java.util.Scanner;
+
+public class Task14 {
+    public static void main(String[] args) throws Exception {
+        Comments c = new Comments();
+        Scanner scanner = new Scanner(System.in);
+        String fpath = "";
+        System.out.print("Введите имя файла с комментариями: ");
+        fpath = scanner.nextLine();
+        c.fillComments(fpath);
+        System.out.print("Введите имя файла с черным списком: ");
+        fpath = scanner.nextLine();
+        c.fillBlackList(fpath);
+        System.out.print("Введите имя файла для записи результатов: ");
+        fpath = scanner.nextLine();
+        c.filter();
+        c.writeResults(fpath);
+        scanner.close();
+    }
+}
