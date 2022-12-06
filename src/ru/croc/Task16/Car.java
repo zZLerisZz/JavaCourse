6c934bfdb1264eb521d9ea825155e73a6a1d997b
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-06 12:00:41 +0300

    Update Car.java

--- src/ru/croc/Task16/Car.java
+++ src/ru/croc/Task16/Car.java
@@ -2,31 +2,29 @@ package ru.croc.Task16;
 
 import java.util.List;
 
-public class Client {
-    private double xcord;
-    private double ycord;
+public class Car {
+    private int num;
+    private String model;
     private String type;
-    private List<String> wlist;
-    public Client(double x, double y, String _type, List<String> wishes){
-        this.xcord = x;
-        this.ycord = y;
+    private List<String> facilities;
+    public Car(int _num, String _model, String _type, List<String> _fac){
+        this.num = _num;
+        this.model = _model;
         this.type = _type;
-        this.wlist = wishes;
-    }
-    public List<String> getWlist(){
-        return wlist;
-    }
-    public double getXcord(){
-        return xcord;
-    }
-    public double getYcord() {
-        return ycord;
+        this.facilities = _fac;
     }
     @Override
     public String toString(){
-        return "[" + xcord + "; " + ycord + "]-" + type + "-" + wlist;
+        return model + "-" + type + "-" + num + "-" + facilities;
+    }
+
+    public List<String> getFacilities(){
+        return facilities;
     }
     public String getType(){
         return type;
+    }
+    public int getNum(){
+        return num;
     }
 }
