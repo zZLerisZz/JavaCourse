007c0aa9593e06d7222a3f9f1621c220ad080c66
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-12 14:38:19 +0300

    Update Filter.java

--- src/ru/croc/Task14/Filter.java
+++ src/ru/croc/Task14/Filter.java
@@ -1,5 +1,28 @@
 package ru.croc.Task14;
 
-public class Filter<T> implements BlackListFilter<T>{
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.function.Predicate;
 
+public class Filter<T> implements BlackListFilter<T>{
+    Set<String> badWords = new HashSet<>();
+    public Set<String> getBadWords(){
+        return badWords;
+    }
+    public void fillBlackList(String filepath) throws Exception{
+        Scanner scan = null;
+        FileReader fl = null;
+        fl = new FileReader(filepath);
+        scan = new Scanner(fl);
+        while(scan.hasNextLine())
+            badWords.add(scan.nextLine());
+        scan.close();
+        fl.close();
+    }
+    public List<T> censorComments(List<T> commentsToCensor, Predicate<T> censor){
+        return filterComments(commentsToCensor, censor);
+    }
 }
