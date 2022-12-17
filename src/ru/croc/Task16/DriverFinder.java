3122815db2a5f1d1149335bfcb36058fbbfb8c1f
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-17 16:51:57 +0300

    Update DriverFinder.java

--- src/ru/croc/Task16/DriverFinder.java
+++ src/ru/croc/Task16/DriverFinder.java
@@ -1,36 +1,9 @@
 package ru.croc.Task16;
 
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 public class DriverFinder {
-    List<Driver> drivers;
-    Client cl;
-    public DriverFinder(String fpath, Client _cl) throws Exception{
-        cl = _cl;
-        drivers = new ArrayList<>();
-        Scanner scan = null;
-        FileReader fl = null;
-        fl = new FileReader(fpath);
-        scan = new Scanner(fl);
-        List<String> str = new ArrayList<>();
-        while (scan.hasNextLine())
-            str.add(scan.nextLine());
-        String[] nstr = str.toArray(new String[str.size()]);
-        for(int i = 0; i < str.size(); i++){
-            String[] cstr = nstr[i].split(";");
-            drivers.add(new Driver(cstr[0], Double.parseDouble(cstr[1]), Double.parseDouble(cstr[2]),
-                    new Car(Integer.parseInt(cstr[3]), cstr[4], cstr[5],
-                            new ArrayList<>(Arrays.asList(Arrays.copyOfRange(cstr, 6, cstr.length))))));
-        }
-        drivers.sort(Driver.driverComparator);
-        scan.close();
-        fl.close();
-    }
-    public void findDriver(){
+    public Driver findDriver(List<Driver> drivers, Client cl){
         Driver d = null;
         double ras = -1;
         boolean fl;
@@ -42,17 +15,14 @@ public class DriverFinder {
                     break;
                 }
             }
-            if(fl && it.getType().equals(cl.getType()) && (ras < 0 || findDistance(it) < ras)){
-                ras = findDistance(it);
+            if(fl && it.getType().equals(cl.getType()) && (ras < 0 || findDistance(it, cl) < ras)){
+                ras = findDistance(it, cl);
                 d = it;
             }
         }
-        if(d == null)
-            System.out.println("Подходящий таксист не найден.");
-        else
-            System.out.println("Вам подходит: " + d);
+        return d;
     }
-    private double findDistance(Driver d){
+    private double findDistance(Driver d, Client cl){
         return Math.sqrt(Math.pow(d.getXcord() - cl.getXcord(), 2) + Math.pow(d.getYcord() - cl.getYcord(), 2));
     }
 }
