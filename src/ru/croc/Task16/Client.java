49cb7b2fd4e1291e01693ff54a8f77fb3137fccf
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-17 16:51:18 +0300

    Update Client.java

--- src/ru/croc/Task16/Client.java
+++ src/ru/croc/Task16/Client.java
@@ -3,10 +3,10 @@ package ru.croc.Task16;
 import java.util.List;
 
 public class Client {
-    private double xcord;
-    private double ycord;
-    private String type;
-    private List<String> wlist;
+    private final double xcord;
+    private final double ycord;
+    private final String type;
+    private final List<String> wlist;
     public Client(double x, double y, String _type, List<String> wishes){
         this.xcord = x;
         this.ycord = y;
