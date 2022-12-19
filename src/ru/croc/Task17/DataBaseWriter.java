025006344756577e68875b3a3463d30af49e838d
zZLerisZz <116449542+zZLerisZz@users.noreply.github.com>
2022-12-19 17:31:59 +0300

    Update DataBaseWriter.java

--- src/ru/croc/Task17/DataBaseWriter.java
+++ src/ru/croc/Task17/DataBaseWriter.java
@@ -1,48 +1,39 @@
 package ru.croc.Task17;
+
 import java.sql.*;
-import java.util.List;
 
 public class DataBaseWriter {
-    public void fillDataBase(String filepath, String username, String password, List<String> data){
-        try(Connection conn = DriverManager.getConnection(filepath, username, password)){
-            Statement statement = conn.createStatement();
-            statement.execute("create table if not exists items(vendorCode varchar not null, " +
-                    "usname varchar not null, price int not null)");
-            statement.execute("create table if not exists orders(num int not null, " +
-                    "vendorCode varchar not null, buyer varchar not null)");
-            statement.execute("delete from items");
-            statement.execute("delete from orders");
-            for(var it:data){
-                String[] temp = it.split(",");
-                int num = Integer.parseInt(temp[0]);
-                String name = temp[1];
-                String vendor = temp[2];
-                String item = temp[3];
-                int price = Integer.parseInt(temp[4]);
-                statement.execute("INSERT INTO orders VALUES" + "(" + num + ", '"  + vendor + "', '" + name + "')");
-                if(isUnique(conn, vendor)){
-                    statement.execute("INSERT INTO items VALUES" + "('" + vendor + "', '" + item + "', " + price + ")");
+    private final Connection conn;
+    public DataBaseWriter(String url, String username, String password) throws SQLException {
+        conn = DriverManager.getConnection(url, username, password);
+    }
+    public void fillDataBase(OrderDataBase baseToWrite) throws SQLException {
+        Statement statement = conn.createStatement();
+        statement.execute("create table if not exists items(vendorCode varchar primary key, " +
+                    "item_name varchar not null, price int not null)");
+        statement.execute("create table if not exists orders(num int primary key, " +
+                    "vendor_Codes varchar not null, buyer varchar not null)");
+        statement.execute("delete from items");
+        statement.execute("delete from orders");
+        for(var it:baseToWrite.getOrderList()){
+            statement.execute("INSERT INTO orders VALUES" + "(" + it.getNumberOfOrder() + ", '"
+                        + it.getVendorCodes() + "', '" + it.getUserName() + "')");
+            for(var item : it.getItemList())
+                if(isUnique(conn, item.getVendorCode())){
+                    statement.execute("INSERT INTO items VALUES" + "('" + item.getVendorCode() + "', '" +
+                        item.getItemName() + "', " + item.getPrice() + ")");
                 }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
     }
-    private boolean isUnique(Connection c, String vcode){
+    private boolean isUnique(Connection c, String vcode) throws SQLException {
         String select = "SELECT * FROM items WHERE vendorCode = '" + vcode + "'";
-        try (Statement statement = c.createStatement()){
-            try (ResultSet result = statement.executeQuery(select)) {
-                if (result.next()) {
-                    return false;
-                }
-                else {
-                    return true;
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        Statement statement = c.createStatement();
+        ResultSet result = statement.executeQuery(select);
+        if (result.next()) {
+            return false;
+        }
+        else {
+            return true;
         }
     }
 }
